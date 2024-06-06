dea268dac80797a12cbdb805e278b690e502ca11
Andrey-6oyko <andreyboyko200032@gmail.com>
2024-06-06 08:43:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    var selectedColor = document.getElementById('colorPicker').value;
+    document.body.style.backgroundColor = selectedColor;
+});
